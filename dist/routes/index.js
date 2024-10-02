8b5b3631235073cc12aa4bebf09a5038c10f92eb
ACAZUMAH <calebazumah9@gmail.com>
2024-10-02 14:27:19 +0000

    deployment

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -7,6 +7,9 @@ const express_1 = require("express");
 const auth_route_1 = __importDefault(require("./auth-route"));
 const jobs_route_1 = __importDefault(require("./jobs-route"));
 const router = (0, express_1.Router)();
+router.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the job portal API' });
+});
 router.use('/api', auth_route_1.default);
 router.use('/api', jobs_route_1.default);
 exports.default = router;
